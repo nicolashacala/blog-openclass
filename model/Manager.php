eee3960c0e5e23d163bca01cb8d5f1144905df88
nicolashacala <nicolas.hacala@gmail.com>
2018-02-12 14:41:49 +0100

    db added

--- model/Manager.php
+++ model/Manager.php
@@ -3,7 +3,7 @@ namespace Openclassroom\Blog\Model;
 
 class Manager{
     protected function dbConnect(){
-        $db = new \PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+        $db = new \PDO('mysql:host=ec2-54-247-101-202.eu-west-1.compute.amazonaws.com;dbname=d5qef9ckbdct2t;charset=utf8', 'tnagorymyvbuuu', 'a01f31996db8ef0a61459d37f5e66fd7a39cd1f6e8df5a8f718e4fbec30f5e46');
         
         return $db;
     }
